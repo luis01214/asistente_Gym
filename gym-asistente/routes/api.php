e2658c7ab3256562a64c7459eddabc8e34dcc1b8
Luis Eduardo <luisdduardo8gv@gmail.com>
2025-06-01 21:26:41 -0600

    delete no necesarios

--- gym-asistente/routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-use App\Http\Controllers\UserController;
-
-Route::get('/usuarios', [UserController::class, 'index']); // Listar usuarios (opcional)
-Route::post('/usuarios', [UserController::class, 'store']); // Crear usuario
